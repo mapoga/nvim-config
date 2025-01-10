1fcc03303f5075c7c9f03eae8d98abecd4e922bf
Mathieu Goulet-Aubin <mathieugouletaubin@gmail.com>
2025-01-10 04:59:18 -0500

    Added LazyGit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -147,4 +147,24 @@ return {
             view = { adaptive_size = true },
         },
     },
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+        },
+    },
 }
