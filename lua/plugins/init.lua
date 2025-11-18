3a557bcfb8590b05118fbc641d1712e4a9a5d0e9
Mathieu Goulet-Aubin <mathieugouletaubin@gmail.com>
2025-11-17 21:57:58 -0500

    feat: add plugin to split/join: list, dict, ...

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,6 +5,17 @@ return {
     },
 
     {
+        "Wansmer/treesj",
+        keys = { "<space>m", "<space>j", "<space>s" },
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("treesj").setup {
+                max_join_length = 80,
+            }
+        end,
+    },
+
+    {
         "stevearc/conform.nvim",
         event = "BufWritePre",
         config = function()
