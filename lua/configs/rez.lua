c329f4c3eea8d49a1011704e5dce07a9bf30f2bd
Mathieu Goulet-Aubin <mathieugouletaubin@gmail.com>
2026-04-07 20:28:31 -0400

    fix: improve rez module

--- lua/configs/rez.lua
+++ lua/configs/rez.lua
@@ -1,5 +1,10 @@
-local function build()
-    -- Update progress message every tick of the spinner.
+local Path = require "plenary.path"
+
+local function build_cwd()
+    -- Run "rez build -ic" on the current working directory.
+
+    -- Updates the progress message every 10th of a second.
+    -- This is to animate the spinner.
     spinner = require("configs.spinner").Spinner:new {
         on_incr = function(opts)
             local spin_str = opts.spinner:frame()
@@ -7,82 +12,86 @@ local function build()
         end,
     }
 
-    -- Command
-    -- local cmd = { "sleep", "2" }
+    -- Launch command in the background.
     local cmd = { "rez", "build", "-ic" }
+    local errors = ""
+    local result = vim.fn.jobstart(cmd, {
 
-    -- Launch in Background
-    local stack_trace = ""
-    vim.fn.jobstart(cmd, {
-
-        -- Accumulate error messages
+        -- Accumulate errors
         on_stderr = function(job_id, data, event)
-            table.remove(data)
-            stack_trace = stack_trace .. "\n" .. table.concat(data, "\n")
+            table.remove(data) -- Remove the last element since its always empty.
+            errors = errors .. "\n" .. table.concat(data, "\n")
         end,
 
-        -- Stop spinner and print final message.
-        on_exit = function(id, exitcode, event)
+        -- Stop the spinner and print the final message.
+        on_exit = function(id, exit_code, event)
             spinner:stop()
-            if exitcode == 0 then
+            if exit_code == 0 then
                 vim.api.nvim_echo({ { " Package Build Complete ", "DiagnosticOk" } }, true, {})
             else
-                vim.notify(stack_trace .. "\n" .. " Package Build Failed ", vim.log.levels.ERROR)
+                vim.notify(errors .. "\n" .. " Package Build Failed ", vim.log.levels.ERROR)
             end
         end,
     })
 
-    -- Only start if the command did not throw an error right away.
-    spinner:start()
+    -- Only start the animation if the command did not throw an error right away.
+    if result > 0 then
+        spinner:start()
+    end
 end
 
-local function get_package_name(callback)
-    -- This is async because i could not find a better way to get clean results.
+local function get_cwd_name(callback)
+    -- Returns the package name of the current source directory.
+    -- Currently async because i could not find a better way to get clean outputs.
     -- There is room for improvement.
+
+    -- Pipes:
     -- 1. Print the content of pacakge.py
     -- 2. Find the first line starting with: "name ="
     -- 3. Remove the part before the name
     -- 4. Remove the part after the name
-    local name_cmd =
+    local cmd =
         [[ rez-build --view-pre | grep -m 1 "name\s*=\s*.*" | sed "s/^name\s*=\s*['\"]//" | sed "s/\s*['\"]\s*$//"]]
 
-    local pkg_name = nil
-    vim.fn.jobstart(name_cmd, {
+    local name = nil
+    vim.fn.jobstart(cmd, {
 
         -- Store the package name
         on_stdout = function(job_id, data, event)
             table.remove(data)
             if #data == 1 then
-                pkg_name = data[1]
+                name = data[1]
             end
         end,
 
         -- Return value
-        on_exit = function(id, exitcode, event)
-            callback(pkg_name)
+        on_exit = function(id, exit_code, event)
+            callback(name)
         end,
     })
 end
 
-local function clear_build()
+local function clear_cwd_build()
+    -- Removes the local build of the current source directory.
+
     vim.notify("Removing build ...", vim.log.levels.INFO)
-
-    local pkg_name = nil
-    get_package_name(function(name)
-        pkg_name = name
-
-        if not pkg_name then
-            vim.notify(" Could not get name of package at: " .. vim.fn.getcwd(), vim.log.levels.ERROR)
+    get_cwd_name(function(name)
+        -- Package name could not be found.
+        -- Most likely not in a source directory.
+        -- Abort.
+        if not name then
+            vim.notify(" Could not find source package at: " .. vim.fn.getcwd(), vim.log.levels.ERROR)
             return
         end
 
-        vim.notify("Package Name: " .. pkg_name, vim.log.levels.INFO)
-        local plen_path = require "plenary.path"
-        local build_path = "~/packages/" .. pkg_name
-        build_path = plen_path:new(build_path):expand()
+        -- Get the local build path.
+        vim.notify("Package Name: " .. name, vim.log.levels.INFO)
+        local build_path = "~/packages/" .. name
+        build_path = Path:new(build_path):expand()
         vim.notify("Build Path: " .. build_path, vim.log.levels.INFO)
 
-        if plen_path:new(build_path):is_dir() then
+        -- Remove the directory if it exists.
+        if Path:new(build_path):is_dir() then
             vim.fn.system { "rm", "-rf", build_path }
             vim.api.nvim_echo({ { " Build Removed: " .. build_path, "Normal" } }, true, {})
         else
@@ -91,4 +100,4 @@ local function clear_build()
     end)
 end
 
-return { build = build, clear_build = clear_build }
+return { build_cwd = build_cwd, clear_cwd_build = clear_cwd_build }
